00473040940dc7c54d249f25de91a86f405ec56d
Gaston ortega <59257791+gortega87@users.noreply.github.com>
2023-04-17 10:44:37 -0300

    Update provider.tf

--- templates/tf1/provider.tf
+++ templates/tf1/provider.tf
@@ -2,16 +2,17 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.62.0"
+     
     }
     datadog = {
       source = "DataDog/datadog"
-      version = "3.23.0"
+     
     }
   }
 }
 
 provider "aws" {
+  region = var.region
   assume_role {
     role_arn = var.role
   }
